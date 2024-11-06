555ead5309bc35a21407250429da7aa8a5debb1a
chaoyuepan <chaoyuepan@baidu.com>
2024-11-06 15:24:23 +0800

    add gateway querying

--- route/gateway.go
+++ route/gateway.go
@@ -48,3 +48,58 @@ func GetMacAddrFromArpCache(ip string) (string, error) {
 	}
 	return "", fmt.Errorf("MAC address not found for IP: %s", ip)
 }
+
+// Use Route
+//
+// // getGatewayIPAndMAC returns the gateway IP and MAC address for a given remote IP address.
+// func getGatewayIPAndMAC(remoteIP string) (string, string, error) {
+// 	// Get the list of network interfaces
+// 	interfaces, err := net.Interfaces()
+// 	if err != nil {
+// 		return "", "", err
+// 	}
+
+// 	// Resolve the remote IP address
+// 	ip := net.ParseIP(remoteIP)
+// 	if ip == nil {
+// 		return "", "", fmt.Errorf("invalid remote IP address: %s", remoteIP)
+// 	}
+
+// 	// Iterate through the interfaces to find the correct gateway
+// 	for _, iface := range interfaces {
+// 		// Filter out non-up interfaces and loopback interfaces
+// 		if iface.Flags&net.FlagUp == 0 || iface.Flags&net.FlagLoopback != 0 {
+// 			continue
+// 		}
+
+// 		// Get the interface's addresses
+// 		addrs, err := iface.Addrs()
+// 		if err != nil {
+// 			continue
+// 		}
+
+// 		for _, addr := range addrs {
+// 			// Only handle IPv4 addresses
+// 			if ipNet, ok := addr.(*net.IPNet); ok && ipNet.IP.To4() != nil {
+// 				// Check if the remote IP is in the same subnet as the interface
+// 				if ipNet.Contains(ip) || ipNet.IP.Equal(ip) {
+// 					// If the remote IP is in the same subnet, get its MAC address
+// 					macAddr, err := GetMacAddress(remoteIP)
+// 					if err == nil {
+// 						return "", macAddr, nil // Return MAC address if found
+// 					}
+
+// 					// If not, get the gateway IP for this interface
+// 					gatewayIP, err := GetGatewayIP(iface.Name)
+// 					if err != nil {
+// 						return "", "", err
+// 					}
+
+// 					macAddr = iface.HardwareAddr.String() // MAC address of the interface
+// 					return gatewayIP, macAddr, nil
+// 				}
+// 			}
+// 		}
+// 	}
+// 	return "", "", fmt.Errorf("no suitable gateway found for remote IP: %s", remoteIP)
+// }
